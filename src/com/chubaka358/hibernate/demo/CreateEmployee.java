e25128edd4df907b7ee63fe4f7d6301c90740b0a
Dmitry Krasnov <chubaka358@icloud.com>
2019-11-02 22:47:03 +0300

    small fixes

--- src/com/chubaka358/hibernate/demo/CreateEmployee.java
+++ src/com/chubaka358/hibernate/demo/CreateEmployee.java
@@ -4,12 +4,8 @@ import com.chubaka358.console.ConsoleHelper;
 import com.chubaka358.hibernate.demo.entity.Employee;
 import com.chubaka358.session.SessionHelper;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class CreateEmployee {
 
